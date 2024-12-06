530f5ce84e9c6f88cdc4a53b3a3a25a8172c81e1
marwan salem <marwansalem01274087424@gmail.com>
2024-12-06 20:15:11 +0200

    i finish routing,screenutil,fonts for Arabic & English, add extension for navigation on context

--- lib/asroo_store_app.dart
+++ lib/asroo_store_app.dart
@@ -1,9 +1,13 @@
 import 'package:asroo_store/core/app/connectivity_controller.dart';
 import 'package:asroo_store/core/common/screens/no_network_screen.dart';
+import 'package:asroo_store/core/routes/app_router.dart';
+import 'package:asroo_store/core/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AsrooStoreApp extends StatelessWidget {
-  const AsrooStoreApp({super.key});
+  const AsrooStoreApp({required this.appRouter, super.key});
+  final AppRouter appRouter;
 
   // This widget is the root of your application.
   @override
@@ -12,21 +16,25 @@ class AsrooStoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, bool networkStatus, __) {
         if (networkStatus) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Builder(builder: (context) {
-                ConnectivityController.instance.init();
-                return widget!;
-              });
-            },
-            home: Container(
-              child: Text('Home'),
+          return ScreenUtilInit(
+            designSize: const Size(390, 844),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, widget) {
+                return Builder(builder: (context) {
+                  ConnectivityController.instance.init();
+                  return widget!;
+                });
+              },
+               onGenerateRoute: appRouter.generateRoute,
+            initialRoute: Routes.testOneScreen,
             ),
           );
         } else {
---
+++ lib/core/style/fonts/font_family_helper.dart
@@ -0,0 +1,6 @@
+class FontFamilyHelper {
+  const FontFamilyHelper._();
+  static const String cairoArabic='cairo';
+  static const String poppinsEnglish='poppins';
+  //TODO: switch anglish & Arabic
+}
